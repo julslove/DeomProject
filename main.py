84b5b02a50b777bff2b13ea67a6a733398dbfa18
juls_bel <87903285+julslove@users.noreply.github.com>
2023-04-30 17:31:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-s= 'ufdfvfvnfjvn'
+"""
+This file contains call functions to final project of Git course for beggineers.
+"""
+
+
+
+def main():
+    s1 = "saippuakivikauppias" 
+    s2 = "Malayalam"
+    s3 = "BaTHTuB"
+    s4 = "ontotonto"
+    s5 = "detartrated"
+    
+    // create here fucntion call to your fucntion.
+    // create here fucntion call to your fucntion.
+    // create here fucntion call to your fucntion.
+    // create here fucntion call to your fucntion.
+    // create here fucntion call to your fucntion.   
+    
+    
+    print("Congratulations on completing the Git course for beginners!")
+
+if __name__ == "__main__":
+    main()
