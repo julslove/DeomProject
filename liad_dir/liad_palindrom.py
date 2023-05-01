04abecdcb26d5aa140946c4bc79afd67858398ef
Liad Nachlai <liad.nachlai@gmail.com>
2023-05-01 21:02:50 +0300

    Added new file with palindrom question solution

---
+++ liad_dir/liad_palindrom.py
@@ -0,0 +1,8 @@
+'''
+This is a file solving the palindrom problem
+'''
+
+
+def is_palindrom(src_str: str) -> bool:
+     return src_str == src_str[::-1]
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 This file contains call functions to final project of Git course for beggineers.
 """
 
-
+from liad_dir.liad_palindrom import is_palindrom
 
 def main():
     s1 = "saippuakivikauppias" 
@@ -11,11 +11,11 @@ def main():
     s4 = "ontotonto"
     s5 = "detartrated"
     
-    // create here fucntion call to your fucntion.
-    // create here fucntion call to your fucntion.
-    // create here fucntion call to your fucntion.
-    // create here fucntion call to your fucntion.
-    // create here fucntion call to your fucntion.   
+    is_palindrom(s1)
+    is_palindrom(s2)
+    is_palindrom(s3)
+    is_palindrom(s4)
+    is_palindrom(s5)
     
     
     print("Congratulations on completing the Git course for beginners!")
