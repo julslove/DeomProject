5266678660c6ebfa91bc553b45ea717d2e1198f8
etJoy <ettymalka@gmail.com>
2023-05-15 20:21:31 +0300

    remove test line

--- etty_dir/etty_palindrom.py
+++ etty_dir/etty_palindrom.py
@@ -3,10 +3,10 @@
 # the function will return a string in reverse mode
 def isPalindrom(s):
     #print (s)      prints s for checking
-    #print (s[::-1])  prints the reverse of the str
+    #print (s[::-1])  prints the reverse of the str for checking
     return s.lower() == s[::-1].lower()
 
-s = "detartrated"
+
 result = isPalindrom(s)
 
 if result:
